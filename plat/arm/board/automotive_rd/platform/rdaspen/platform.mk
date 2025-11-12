0dfdcacce91e05c6175dc2867a69b33b38b5f1bf
Akshay Godase <Akshay.Godase@arm.com>
2025-11-12 18:04:04 +0530

    fix(rdaspen): Configure PFDI source to avoid patching issue
    Fixes patching issue observed in yocto build with declaration
    of pfdi source that can be over-ridden.
    
    Upstream-Status: Pending
    
    Signed-off-by: Akshay Godase <Akshay.Godase@arm.com>

--- plat/arm/board/automotive_rd/platform/rdaspen/platform.mk
+++ plat/arm/board/automotive_rd/platform/rdaspen/platform.mk
@@ -62,8 +62,9 @@ EL3_EXCEPTION_HANDLING			:=	1
 FAULT_INJECTION_SUPPORT			?=	1
 
 ifeq (${PFDI_SUPPORT}, 1)
-BL31_SOURCES		+=	${RDASPEN_BASE}/rdaspen_pfdi.c \
-				${RDASPEN_BASE}/rdaspen_sbist.c
+PFDI_SOURCE 		?=	${RDASPEN_BASE}/rdaspen_pfdi.c
+BL31_SOURCES		+=	${RDASPEN_BASE}/rdaspen_sbist.c \
+				${PFDI_SOURCE}
 endif
 
 
